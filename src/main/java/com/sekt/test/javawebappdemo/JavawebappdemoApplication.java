d60825043a25511ebd9eee5c5965cf67cd3fdb36
Sekar Thangavel <sekar.citrixbox@gmail.com>
2025-01-22 23:12:45 -0500

    Initial web app files

---
+++ src/main/java/com/sekt/test/javawebappdemo/JavawebappdemoApplication.java
@@ -0,0 +1,13 @@
+package com.sekt.test.javawebappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawebappdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavawebappdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sekt/test/javawebappdemo/JavawebappdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekt.test.javawebappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavawebappdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
